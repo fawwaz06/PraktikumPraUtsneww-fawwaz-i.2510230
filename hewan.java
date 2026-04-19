f1482d9874bb9cbea010946c4ad1070622d9bce1
fawwaz06 <muhammadfawazazmi0507@gmail.com>
2026-04-19 20:19:29 +0700

    pratkum

---
+++ hewan.java
@@ -0,0 +1,17 @@
+public class Hewan {
+    void suara() {
+        System.out.println("Suara hewan");
+    }
+}
+
+class Herbivora extends Hewan {
+    void suara() {
+        System.out.println("Suara herbivora");
+    }
+}
+
+class Karnivora extends Hewan {
+    void suara() {
+        System.out.println("Suara karnivora");
+    }
+}
